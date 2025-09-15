71da149a2151186b4000d2365615141b7c202a23
Griehle <briahsdad@yahoo.com>
2025-09-14 20:09:48 -0600

    Init Commit

---
+++ src/app/(frontend)/register/page.tsx
@@ -0,0 +1,11 @@
+import RegisterForm from '@/components/RegisterForm'
+
+export default function RegisterPage() {
+  return (
+    <div className="auth-page">
+      <div className="auth-container">
+        <RegisterForm />
+      </div>
+    </div>
+  )
+}
